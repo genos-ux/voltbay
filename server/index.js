f88db815ba58616726e8c953b85e405e689e855c
mrhihihi <gabriel.kyeremateng.gk@gmail.com>
2025-05-20 01:39:59 +0000

    registered middlewares

--- server/index.js
+++ server/index.js
@@ -1,6 +1,18 @@
 import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan"
 
 const app = express();
 
+// register middlewares.
+app.use(express.json());
+app.use(cors());
+app.use(helmet()); // helmet is a security middleware that helps you protect your app by setting various HTTP headers.
+app.use(morgan("dev")) // log the requests.
+
+app.get('/',(req,res) => {
+    res.send("Hello from the backend");
+})
 
 app.listen(4000, () => console.log("Server is running on port 4000"));
