12bb42e802a070c426ad38054cb564f1e75ce294
mrhihihi <gabriel.kyeremateng.gk@gmail.com>
2025-05-26 19:55:55 +0000

    added the frontend

---
+++ app/src/pages/ProductPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ProductPage() {
+  return (
+    <div>
+      <h2>ProductPage</h2>
+    </div>
+  )
+}
+
+export default ProductPage
---
+++ app/tailwind.config.js
@@ -0,0 +1,26 @@
+import daisyui from "daisyui";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui],
+  daisyui: {
+    themes: [
+      "forest",
+      "aqua",
+      "lofi",
+      "pastel",
+      "fantasy",
+      "wireframe",
+      "black",
+      "luxury",
+      "dracula",
+      "cmyk",
+      "autumn",
+      "business",
+    ],
+  },
+};
